0e20f624813b65d03c1cf5c539807ae2ffc89385
Nanlung <nanlung.temlong@gmail.com>
2022-03-02 18:57:45 +0100

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+export const INCREMENT = "increment";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
+
+export default store;
